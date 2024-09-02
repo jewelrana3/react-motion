0d8018eee71bdb9fe00cdd48a174ec895a464310
juyel haque <juyelrana7752@gmail.com>
2024-09-02 11:34:10 -0700

    Back button style and outlet add

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,6 +7,13 @@ export default function Button() {
         className="bg-black text-white p-1 border"
         whileTap={{ scale: 1.5 }}
         whileHover={{ scale: 2, transition: { yoyo: Infinity } }}
+        exit={{
+          x: "-100vw",
+          transition: {
+            duration: 1,
+            ease: "easeInOut",
+          },
+        }}
       >
         Tap me
       </motion.button>
